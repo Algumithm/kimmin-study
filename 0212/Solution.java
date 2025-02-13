a3206b8aed1901f051705cf1fe5ac52c77bc9ce9
kim0904min <kim0904min@gmail.com>
2025-02-13 09:30:46 +0900

    주석 추가

--- 0212/Solution.java
+++ 0212/Solution.java
@@ -7,10 +7,10 @@ import java.util.Scanner;
 
 class Solution {
 
-	static int N;
-	static int M;
-	static int K;
-	static int BORDER;
+	static int N;  // 공간 n 상수선언
+	static int M;  // 시간 m 상수선언
+	static int K;  // 군집 수 k 상수선언
+	static int BORDER;  // 약품 처리 영역 중 n - 1 을 border로 상수선언
 
 	static class Micro {
 		int y;
@@ -23,7 +23,7 @@ class Solution {
 			this.x = x;
 			this.num = num;
 			this.dir = dir;
-		}
+		}  // 입력받은 y, x, num, dir 순서대로 객체 생성
 
 	// @Override
 	// 	public String toString() {
@@ -32,6 +32,7 @@ class Solution {
 	}
 	
 	static int changeDir(Micro micro) {
+		// micro 객체의 방향 전환을 위한 switch-case문
 			switch (micro.dir) {
 			case 1:
 				return 2;
@@ -47,6 +48,7 @@ class Solution {
 	}
 	
 	static void checkBorder(Micro micro) {
+		// micro 객체가 약품 영역에 도달할 시 반감 후 방향 전환 함수 호출
 		if(micro.x == 0 ||  micro.x == BORDER || micro.y == 0 ||  micro.y == BORDER) {
 			micro.num /= 2;
 			micro.dir = changeDir(micro);
@@ -54,6 +56,7 @@ class Solution {
 	}
 	
 	static void moveMicro(Micro micro) {
+		// micro 객체 이동 후 약품 영역 위치 확인 함수 호출
 		switch (micro.dir) {
 			case 1: {
 				micro.y--;
@@ -90,6 +93,8 @@ class Solution {
 			
 			BORDER = N - 1;
 			
+			// 케이스 별 선 입력 값을 상수에 대입
+			
 			int total = 0;
 
 			List<Micro> microList = new ArrayList<>();
@@ -99,38 +104,46 @@ class Solution {
 				int num = sc.nextInt();
 				int dir = sc.nextInt();
 				microList.add(new Micro(y, x, num, dir));
-//      System.out.println(microList.get(k));
-			}
+                // System.out.println(microList.get(k));
+			} // 해당 케이스의 군집 수 만큼 micro 객체 생성 후 ArrayList에 삽입
 			
 			microList.sort(Comparator.comparingInt((Micro m) -> m.num).reversed());
+			// microList의 num값을 기준으로 내림차순 정렬
 			
 			for (int m = 0; m < M; m++) {
 				for (int k = 0; k < microList.size(); k++) {
 					moveMicro(microList.get(k));
-				}
+				} // 군집 전체 이동 작업 수행
 				
 				int size = microList.size();
 				
 				for (int k = 0; k < size - 1; k++) {
 					for(int l = k; l < size; l++) {
+						// k와 k ~ BORDER 사이에 존재하는 micro 객체 비교
 						Micro first = microList.get(k);
 						Micro second = microList.get(l);
 						
 						if (microList.get(k).equals(microList.get(l)))
 							continue;
+						// 만약 같은 객체를 비교할 시 continue
 						
-						if(first.y == second.y
-								&& first.x == second.x) {
+						if(first.y == second.y && first.x == second.x) {
+							// 군집들이 같은 위치에 존재할 시
 							if (first.num > second.num) {
+								// k쪽이 클 경우 k쪽으로 군집 흡수
 								first.num += second.num;
 								microList.remove(l);
 								l--;
+								// 삭제당한 객체만큼 l감소
 							} else {
+								// l쪽이 클 경우 l쪽으로 군집 흡수
 								second.num += first.num;
 								microList.remove(k);
 								l = k;
+								// k가 삭제당했으므로 k for루프 재수행
 							}
 						size--;
+						// 군집이 삭제됐으므로 전체 사이즈 축소
 						}
 					}
 			}
@@ -139,13 +152,13 @@ class Solution {
 						microList.remove(k);
 						k--;
 					}
-				}
+				} // 잔존 군집 중 0인 객체가 존재 시 삭제
 			}
 			
 			
 			for (int k = 0; k < microList.size(); k++) {
 				total += microList.get(k).num;
-			}
+			} // 잔존 군집들의 크기 합
 			
 			System.out.println("#" + t + " " + total);
 		}
