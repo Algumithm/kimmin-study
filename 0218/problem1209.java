1771316c76d2a007bb9f5910fee983265b90a45e
Kim Min <42553212+ZeppIine@users.noreply.github.com>
2025-02-18 16:33:36 +0900

    Update problem1209.java

--- 0218/problem1209.java
+++ 0218/problem1209.java
@@ -1,7 +1,7 @@
 import java.io.FileInputStream;
 import java.util.Scanner;
 
-public class problem1209 {
+class Solution {
 	public static void main(String args[]) throws Exception
 	{
 //		System.setIn(new FileInputStream("src/input.txt"));
